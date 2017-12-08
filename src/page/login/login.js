5ff42518bb25d7dd3cd8be6f830cb380183bcc56
tacochen <kevin09662016@gmail.com>
2017-12-08 15:12:58 +0800

    feat(layout):main content & footer layout

--- src/page/login/login.js
+++ src/page/login/login.js
@@ -1,5 +1,10 @@
 require('./login.scss');
 
+$('.reg-form').off('submit').on('submit', () => {
+  $('.reg-error').css('margin-top', 0);
+  return false;
+});
+
 if (module.hot) {
   // 當狀態改變(任何檔案儲存)就重整頁面，密集修改 css, ejs 時可以暫時使用這方式自動重整
   // 生產階段發佈時這些會被自動移除
